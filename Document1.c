c2fff761f46ab61519f9f54af3e5b780f41f6a2c
Romanticme <149561602+Romanticme@users.noreply.github.com>
2023-11-07 20:54:55 +0800

    Update Document1.c

--- Document1.c
+++ Document1.c
@@ -8,7 +8,13 @@ int main() {
 	getchar();
 	printf("Are you Sure?:");
 	int judge = getchar();
-	getchar();
+	getchar();//只写这个的话只会吸收一个空格
+	/*
+ 	int ch = 0;
+	while ((ch = getchar()) != '\n') {
+	};
+ 	*/
+	//如果在代码中将getchar（）换成上述代码，则可以使输入密码时有空格不会导致空格误输入进判断中，直到回车才会判断
 	if (judge == 'y') {
 		printf("right");
 	}
